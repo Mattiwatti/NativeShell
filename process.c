7c34dfe0184e1bf3ab4549b709fa2e9bc9d783e6
amdf <787535+amdf@users.noreply.github.com>
2025-01-14 21:30:48 +0300

    proc

--- process.c
+++ process.c
@@ -35,9 +35,22 @@ NTSTATUS CreateNativeProcess(IN PWSTR file_name, IN PWSTR cmd_line, OUT PHANDLE 
 
     status = RtlCreateProcessParameters(&processparameters, &imgname, &dllpath, &dllpath, &cmdline, Env, 0, 0, 0, 0);
 
+    if (processinformation.ImageInformation.SubSystemType != IMAGE_SUBSYSTEM_NATIVE)
+    {
+      RtlCliDisplayString("\nThe %S application cannot be run in native mode.\n", file_name);
+      return STATUS_UNSUCCESSFUL;
+    }
+
     if (!NT_SUCCESS(status))
     {
-        RtlCliDisplayString("RtlCreateProcessParameters failed\n");
+      RtlCliDisplayString("RtlCreateProcessParameters failed\n");
+      return status;
+    }
+
+    status = NtResumeThread(processinformation.ThreadHandle, NULL);
+    if (!NT_SUCCESS(status))
+    {
+        RtlCliDisplayString("NtResumeThread failed\n");
         return status;
     }
 
